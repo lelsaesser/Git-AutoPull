8f72bf82b5c40c50048b0a2bff67fc72913884c9
lelsaesser <lucas.elsaesser@gmail.com>
2018-06-01 10:48:25 +0200

    update
    -runs now as background task
    -displays repository names
    -logs errors correctly

--- script/autopull.ps1
+++ script/autopull.ps1
@@ -1,23 +1,27 @@
 # Git-AutoPull
+# Author D071137
 
 $d_user = $env:UserName
 $log_dir = $PSScriptRoot
 git config --global --unset http.proxy
 git config --global --unset https.proxy
+Write-Host "Guten Morgen Nikolas :)"
 
 while(1){
+    Write-Output(Get-Date) | Tee-Object $log_dir\autopull_logfile.txt
     try { 
         Set-Location -Path C:\Users\$d_user\git
     }  
     catch {
-        Write-Host "Expected path C:\Users\" + $d_user + "\git not found"
+        Write-Output("Expected path C:\Users\" + $d_user + "\git not found") | Tee-Object -Append $log_dir\autopull_logfile.txt
         exit -1
     }
 
-    Write-Output(Get-Date) | Tee-Object $log_dir\autopull_logfile.txt
     Write-Host "starting automated pull..."
     $repos = Get-ChildItem
-    $repos | ForEach-Object {Set-Location -Path C:\Users\$d_user\git\$_ | git pull 2>&1 | Tee-Object -Append $log_dir\autopull_logfile.txt}
+    $repos | ForEach-Object { Write-Output($_.Name) | Tee-Object -Append $log_dir\autopull_logfile.txt; 
+                            Set-Location -Path C:\Users\$d_user\git\$_ | git pull 2>&1 | Tee-Object -Append $log_dir\autopull_logfile.txt; 
+                            Write-Output("`n") | Tee-Object -Append $log_dir\autopull_logfile.txt}
     Write-Host "Script finished. Logfile saved in"$log_dir"\autopull_logfile.txt"
 
     Start-Sleep -Seconds 300
